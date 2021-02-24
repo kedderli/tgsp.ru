4ce49d0783716a1d5e7361d968fa1e79dd2cb9ab
gavrish.me <gavrish.me@yandex.ru>
2021-02-24 13:52:18 +0300

    Свёрстан слайдер на главной

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$('.main-hp-slider-list').slick({
+	dots: true,
+	infinite: false,
+	speed: 400,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dotsClass: 'main-hp-slider-nav',
+	prevArrow: '.main-hp-slider-prev',
+	nextArrow: '.main-hp-slider-next',
+});
