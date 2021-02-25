0e601f67ddf965d4f4b403c7d6affe30f3c01921
gavrish.me <gavrish.me@yandex.ru>
2021-02-25 13:07:05 +0300

    Свёрстан второй блок

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,30 @@ $('.main-hp-slider-list').slick({
 	prevArrow: '.main-hp-slider-prev',
 	nextArrow: '.main-hp-slider-next',
 });
+
+$('.description-cards-list').slick({
+	dots: true,
+	infinite: false,
+	speed: 400,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dotsClass: 'description-slider-nav',
+	prevArrow: '.description-slider-prev',
+	nextArrow: '.description-slider-next',
+	responsive: [
+		{
+			breakpoint: 9999,
+			settings: "unslick"
+		},
+		{
+			breakpoint: 769,
+			settings: {
+				dots: true,
+				infinite: false,
+				speed: 400,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		}
+	]
+});
