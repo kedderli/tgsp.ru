52231e8cf4967576d6a12c7447a590470af4887f
gavrish.me <gavrish.me@yandex.ru>
2021-02-28 22:20:58 +0300

    Свёрстан блок контрактов и хлебные крошки

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// start header
 $(".header-menu-open").click(function() {
 	$("body").toggleClass("no-scroll");
 	$(".header").toggleClass("active");
@@ -15,91 +16,104 @@ $(window).scroll(function() {
 		}
 	}
 });
+// end header
 
-$('.main-hp-slider-list').slick({
-	dots: true,
-	infinite: false,
-	speed: 400,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	dotsClass: 'main-hp-slider-nav',
-	prevArrow: '.main-hp-slider-prev',
-	nextArrow: '.main-hp-slider-next',
-});
+// start main homepage slider
+if (document.querySelector('.main-hp-slider-list')) {
+	$('.main-hp-slider-list').slick({
+		dots: true,
+		infinite: false,
+		speed: 400,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dotsClass: 'main-hp-slider-nav',
+		prevArrow: '.main-hp-slider-prev',
+		nextArrow: '.main-hp-slider-next',
+	});
+}
+// end main homepage slider
 
-$('.description-cards-list').slick({
-	dots: true,
-	infinite: false,
-	speed: 400,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	dotsClass: 'description-slider-nav',
-	prevArrow: '.description-slider-prev',
-	nextArrow: '.description-slider-next',
-	responsive: [
-		{
-			breakpoint: 9999,
-			settings: "unslick"
-		},
-		{
-			breakpoint: 769,
-			settings: {
-				dots: true,
-				infinite: false,
-				speed: 400,
-				slidesToShow: 1,
-				slidesToScroll: 1,
+// start homepage description
+if (document.querySelector('.description-cards-list')) {
+	$('.description-cards-list').slick({
+		dots: true,
+		infinite: false,
+		speed: 400,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dotsClass: 'description-slider-nav',
+		prevArrow: '.description-slider-prev',
+		nextArrow: '.description-slider-next',
+		responsive: [
+			{
+				breakpoint: 9999,
+				settings: "unslick"
+			},
+			{
+				breakpoint: 769,
+				settings: {
+					dots: true,
+					infinite: false,
+					speed: 400,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
 			}
-		}
-	]
-});
+		]
+	});
+}
+// end homepage description
 
-$('.brand-list').slick({
-	dots: false,
-	infinite: false,
-	speed: 400,
-	slidesToShow: 6,
-	slidesToScroll: 6,
-	rows: 4,
-	dotsClass: 'brands-slider-nav',
-	prevArrow: '.brands-slider-prev',
-	nextArrow: '.brands-slider-next',
-	arrows: true,
-	responsive: [
-		{
-			breakpoint: 1200,
-			settings: {
-				rows: 5,
-				slidesToShow: 5,
-				slidesToScroll: 5,
+// start brands list
+if (document.querySelector('.brand-list')) {
+	$('.brand-list').slick({
+		dots: false,
+		infinite: false,
+		speed: 400,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		rows: 4,
+		dotsClass: 'brands-slider-nav',
+		prevArrow: '.brands-slider-prev',
+		nextArrow: '.brands-slider-next',
+		arrows: true,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					rows: 5,
+					slidesToShow: 5,
+					slidesToScroll: 5,
+				}
+			},
+			{
+				breakpoint: 1024,
+				settings: {
+					rows: 2,
+					dots: true,
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					rows: 4,
+					dots: true,
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					rows: 4,
+					dots: true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
 			}
-		},
-		{
-			breakpoint: 1024,
-			settings: {
-				rows: 2,
-				dots: true,
-				slidesToShow: 3,
-				slidesToScroll: 3,
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				rows: 4,
-				dots: true,
-				slidesToShow: 2,
-				slidesToScroll: 2,
-			}
-		},
-		{
-			breakpoint: 500,
-			settings: {
-				rows: 4,
-				dots: true,
-				slidesToShow: 1,
-				slidesToScroll: 1,
-			}
-		}
-	]
-});
+		]
+	});
+}
+// end brands list
